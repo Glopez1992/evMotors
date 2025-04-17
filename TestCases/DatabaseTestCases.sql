8147a506d08a030011a196396fe781f543164da3
Chinu2024 <chinutyagi.ct@gmail.com>
2025-04-17 16:18:26 +0100

    Update DatabaseTestCases.sql

--- TestCases/DatabaseTestCases.sql
+++ TestCases/DatabaseTestCases.sql
@@ -8,7 +8,7 @@ Insert into VehicleRegister values('XYZ123', 'Nissan', '2.1L', '2025-12-31', 200
 Insert into VehicleRegister values('XYZ123', 'Nissan', '2.1L', '2025-03-31', 0.00, 0)
 
 --TC04
-Insert into VehicleRegister values('XYZ123', 'Škoda', '2.1L', '2025-03-31', 300.00, 1)
+Insert into VehicleRegister values('XYZ123', 'Å koda', '2.1L', '2025-03-31', 300.00, 1)
 
 --TC05
 Update VehicleRegister Set Available=0 where VehicleRegNo='ABC123'
@@ -24,7 +24,7 @@ Insert into VehicleRegister values('XYZ789', 'Tesla', '3.1L', '2025-04-01', 500.
 Insert into VehicleRegister values('XYZ456', 'Tesla', NULL, '2025-04-01', 500.00, 1)
 
 --TC09
-Insert into VehicleRegister values('DEF456', 'Ford', '1.5L', '2024-02-01', 80.00)
+Insert into VehicleRegister(VehicleRegNo,Make,EngineSize,DateRegistered,RentalPerDay) values('DEF456', 'Ford', '1.5L', '2024-02-01', 80.00)
 
 --TC10
 Insert into VehicleRegister values('FGH123', 'Nissan', '2.1L', '2025-04-01', 99.999, 1)
