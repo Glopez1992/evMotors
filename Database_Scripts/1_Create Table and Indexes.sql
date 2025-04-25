3c7bac30832397cf3f09158eac46e620d38e25b9
SilverMau <roganmojca@gmail.com>
2025-04-25 12:15:14 +0100

    - Changed Column name from EngineSize to EngineSize/Power - removed electric and replaced with kW in .csv file

--- Database_Scripts/1_Create Table and Indexes.sql
+++ Database_Scripts/1_Create Table and Indexes.sql
@@ -4,12 +4,12 @@ use EVMotors;
 create table VehicleRegister (
 	VehicleRegNo varchar(10) primary key, --Unique identifier and cannot be null // nvarchar - uses special characters like ü, ë, Š,...
 	Make nvarchar(50) not null, --Toyota, Ford, Citroën, Škoda,...
-	EngineSize varchar(10) null, -- can be null for electric vehicles
+	EngineSize varchar(10) null, -- should not be NULL, (fixed below)
 	DateRegistered date not null check(DateRegistered<getdate()), -- registration date is mandatory
 	RentalPerDay decimal(10,2) not null check(RentalPerDay > 0), -- currency format, 2 decimals
 	Available bit not null default 1 -- 1 = Available, 0 = Not Available
 );
-		-- setting Engine size to NOT NULL
+		-- setting Engine size to NOT NULL as I made a mistake while creating a table
 			alter table VehicleRegister
 			alter column EngineSize varchar(10) NOT NULL;
 
@@ -30,18 +30,18 @@ create table VehicleRegister (
 	add constraint PK_VehicleRegNo primary key (VehicleRegNo);
 
 
-
-
-
-
 	-- check for index types
 		SELECT name, type_desc
 		FROM sys.indexes
 		WHERE object_id = OBJECT_ID('VehicleRegister');
 
 
+select * from [dbo].[VehicleRegister];
 
+-------------------------------------------------------------------------------
+-- UPDATE #1
+	-- 1.1 "EngineSize" already set to NOT NULL
 
-
-
+	--	1.2 Change field "EngineSize" to "EngineSize/Power"
+		exec sp_rename 'VehicleRegister.EngineSize', 'EngineSize/Power', 'column';
 
